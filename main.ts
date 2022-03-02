ef173a3a786c62eed6b4f28054535cc09ccf086f
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2022-03-02 17:52:30 +0900

    更新 pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,10 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
+input.onButtonPressed(Button.A, function () {
+    music.startMelody(music.builtInMelody(Melodies.Birthday), MelodyOptions.Once)
     basic.showLeds(`
         . . . . .
-                . # # # .
-                . . # . .
-                . . # . .
-                . . # . .
-    `)
+        . # # # .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
 })
