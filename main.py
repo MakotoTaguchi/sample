5a2a7639bf53a6fef72e0b97a88698206078c154
MakotoTaguchi <83949571+MakotoTaguchi@users.noreply.github.com>
2022-03-02 15:41:00 +0900

    更新 pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    basic.showLeds(`
+def on_button_pressed_a():
+    basic.show_leds("""
         . . . . .
                 . # # # .
                 . . # . .
                 . . # . .
                 . . # . .
-    `)
-})
+    """)
+input.on_button_pressed(Button.A, on_button_pressed_a)
